232db1a87163a47dd4fcea66f29c4aaed7e05f57
Rayest <yi@rayest.cn>
2016-08-28 14:53:14 +0800

    Array 类内置的 sort 排序方法

--- src/cn/rayest/array/Order.java
+++ src/cn/rayest/array/Order.java
@@ -1,5 +1,7 @@
 package cn.rayest.array;
 
+import java.util.Arrays;
+
 /**
  * Created by Rayest on 2016/8/28 0028.
  */
@@ -8,12 +10,16 @@ public class Order {
         int[] data = new int[]{1, 9, 3, 4, 6, 2, 0, 8, 7, 5};
         orderArray(data);
         printArray(data);
+        System.out.println();
+        Arrays.sort(data);
+        printArray(data);
     }
-    public static void orderArray(int[] data){
+
+    public static void orderArray(int[] data) {
         for (int i = 0; i < data.length - 1; i++) { // 循环次数
             for (int j = 0; j < data.length - 1; j++) { // 比较次数
                 int tmp;
-                if (data[j] > data[j + 1]){
+                if (data[j] > data[j + 1]) {
                     tmp = data[j];
                     data[j] = data[j + 1];
                     data[j + 1] = tmp;
@@ -21,7 +27,8 @@ public class Order {
             }
         }
     }
-    public static void printArray(int[] data){
+
+    public static void printArray(int[] data) {
         for (int i = 0; i < data.length; i++) {
             System.out.print(data[i] + " ");
         }
