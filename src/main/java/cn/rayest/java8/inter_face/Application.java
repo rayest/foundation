2c3ca07c421d0fa8546aa6e09965a59d9b153857
Rayest <634178463@qq.com>
2016-11-19 20:57:02 +0800

    接口中定义静态方法

--- src/main/java/cn/rayest/java8/inter_face/Application.java
+++ src/main/java/cn/rayest/java8/inter_face/Application.java
@@ -7,6 +7,7 @@ public class Application {
     public static void main(String[] args) {
         Message message = new MessageImpl();
         message.showDefault(); // 该方法是在接口中直接定义的
-        message.print();
+        message.print(); // 该方法是实现类中定义的
+        Message.showStatic(); // 类名访问接口中定义的接口方法
     }
 }
--- src/main/java/cn/rayest/java8/inter_face/Message.java
+++ src/main/java/cn/rayest/java8/inter_face/Message.java
@@ -2,11 +2,16 @@ package cn.rayest.java8.inter_face;
 
 /**
  * Created by Rayest on 2016/11/19 0019.
+ * default
+ * static
  */
 public interface Message {
     void print();
 
     default void showDefault() {
         System.out.println("接口中定义的普通方法，该方法拥有方法体");
+    }
+    static void showStatic(){
+        System.out.println("接口中定义的静态方法");
     }
 }
