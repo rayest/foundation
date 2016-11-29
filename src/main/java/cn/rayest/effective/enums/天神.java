338a784dd286bc260a4afd3aaa787cb73179b795
Rayest <634178463@qq.com>
2016-11-29 23:36:09 +0800

    枚举的定义和使用

---
+++ src/main/java/cn/rayest/effective/enums/天神.java
@@ -0,0 +1,16 @@
+package cn.rayest.effective.enums;
+
+/**
+ * Created by Rayest on 2016/11/29 0029.
+ */
+public enum 天神 {
+    如来佛祖,
+    玉皇大帝,
+    王母娘娘,
+    太白金星,
+    十八罗汉,;
+
+    public int 天神的顺序() {
+        return ordinal() + 1;
+    }
+}
---
+++ src/main/java/cn/rayest/effective/enums/天神Application.java
@@ -0,0 +1,14 @@
+package cn.rayest.effective.enums;
+
+/**
+ * Created by Rayest on 2016/11/29 0029.
+ */
+public class 天神Application {
+    public static void main(String[] args) {
+        System.out.println(天神.如来佛祖.天神的顺序());
+        System.out.println(天神.玉皇大帝.天神的顺序());
+        System.out.println(天神.王母娘娘.天神的顺序());
+        System.out.println(天神.太白金星.天神的顺序());
+        System.out.println(天神.十八罗汉.天神的顺序());
+    }
+}
