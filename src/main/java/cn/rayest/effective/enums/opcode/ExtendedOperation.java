15cabd1f10f135637e79fbce7d756d7044ba2a79
Rayest <634178463@qq.com>
2016-11-30 22:51:58 +0800

    枚举类的接口实现和扩展

---
+++ src/main/java/cn/rayest/effective/enums/opcode/ExtendedOperation.java
@@ -0,0 +1,28 @@
+package cn.rayest.effective.enums.opcode;
+
+/**
+ * Created by Rayest on 2016/11/30 0030.
+ */
+public enum ExtendedOperation implements Operation {
+    EXP("^") {
+        public double apply(double x, double y) {
+            return Math.pow(x, y);
+        }
+    },
+    REMAINDER("%") {
+        public double apply(double x, double y) {
+            return x % y;
+        }
+    };
+
+    private final String symbol;
+
+    ExtendedOperation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
+}
---
+++ src/main/java/cn/rayest/effective/enums/opcode/Operation.java
@@ -0,0 +1,8 @@
+package cn.rayest.effective.enums.opcode;
+
+/**
+ * Created by Rayest on 2016/11/30 0030.
+ */
+public interface Operation {
+    double apply(double x, double y);
+}
