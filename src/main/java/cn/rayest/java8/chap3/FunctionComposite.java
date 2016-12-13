2890fc71394f2a0f9a27a078a1295768e17ce07c
Rayest <634178463@qq.com>
2016-12-13 22:34:46 +0800

    比较器 andThen 方法

---
+++ src/main/java/cn/rayest/java8/chap3/FunctionComposite.java
@@ -0,0 +1,15 @@
+package cn.rayest.java8.chap3;
+
+import java.util.function.Function;
+
+/**
+ * Created by Rayest on 2016/12/13 0013.
+ */
+public class FunctionComposite {
+    public int getAndThen() {
+        Function<Integer, Integer> f = x -> x + 1;
+        Function<Integer, Integer> g = x -> x * 2;
+        Function<Integer, Integer> h = f.andThen(g);
+        return h.apply(1);
+    }
+}
---
+++ src/test/java/cn/rayest/java8/FunctionCompositeTest.java
@@ -0,0 +1,17 @@
+package cn.rayest.java8;
+
+import cn.rayest.java8.chap3.FunctionComposite;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Rayest on 2016/12/13 0013.
+ */
+public class FunctionCompositeTest {
+    @Test
+    public void testGetAndThen() throws Exception {
+        FunctionComposite functionComposite = new FunctionComposite();
+        assertEquals(4, functionComposite.getAndThen());
+    }
+}
